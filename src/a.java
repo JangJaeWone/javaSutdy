6ead2f2afeb244a3c1a6b859144e8d2a4af1558b
JJW <lokcdown7739@naver.com>
2023-02-09 01:03:58 +0900

    wkdwodnjs

--- src/a.java
+++ src/a.java
@@ -7,10 +7,16 @@ public class a {
         switch(a){
             case 1:
             System.out.println(1);
+            break;
             case 2:
             System.out.println(2);
+            break;
             case 3:
             System.out.println(3);
+            break;
+            default:
+                System.out.println(4);
+                break;
         }
     }
 }
