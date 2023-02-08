97c19a3c9fa045e7d8b8eb8592f23eb799d3725b
JJW <lokcdown7739@naver.com>
2023-02-09 00:38:28 +0900

    wkdwodnjs

--- src/tes5.java
+++ src/tes5.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class tes5 {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner a = new Scanner(System.in);
+        String s = a.next();
+        String d = a.next();
+        if(s.equals("qwer")){
+            if (d.equals("asdf")){
+                System.out.println("aaa");
+            }else {
+                System.out.println("bbb");
+            }
+        }else {
+            System.out.println("bbb");
+        }
     }
 }
