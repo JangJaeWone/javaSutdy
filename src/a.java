93c555e7b9982c55989d74ee9a62e21d76efa8dd
JJW <lokcdown7739@naver.com>
2023-02-09 00:57:18 +0900

    wkdwodnjs

---
+++ src/a.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class a {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        switch(a){
+            case 1:
+            System.out.println(1);
+            case 2:
+            System.out.println(2);
+            case 3:
+            System.out.println(3);
+        }
+    }
+}
