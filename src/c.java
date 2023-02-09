c705bb9f4b53549ee66350070ed3b9f532ec2c96
JJW <lokcdown7739@naver.com>
2023-02-10 00:35:44 +0900

    wkdwodnjs

---
+++ src/c.java
@@ -0,0 +1,8 @@
+public class c {
+    public static void main(String[] args) {
+        for (int a=0; a<10; a++){
+            for (int b=0; b<10; b++)
+                System.out.println(a+""+b);
+        }
+    }
+}
