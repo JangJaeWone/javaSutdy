fe4f837c60f0a01c6e78ab94971ba403553ecc4c
JJW <lokcdown7739@naver.com>
2023-02-11 00:33:19 +0900

    wkdwodnjs

---
+++ src/e.java
@@ -0,0 +1,15 @@
+public class e {
+
+        public static void numbering() {
+            int i = 0;
+                for (i=0; i<10; i++){
+                    System.out.println(i);
+                }
+            }
+    public static void main(String[] args) {
+        numbering();
+        numbering();
+        numbering();
+        numbering();
+    }
+}
